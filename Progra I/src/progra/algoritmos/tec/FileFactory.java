089106c19adccc4b13d100813b90c93037da02e8
Dario <darior1227@gmail.com>
2017-08-27 18:02:45 -0600

    [files]

--- Progra I/src/progra/algoritmos/tec/FileFactory.java
+++ Progra I/src/progra/algoritmos/tec/FileFactory.java
@@ -5,8 +5,6 @@ public class FileFactory {
 	}
 	public FileFactory() {
 		// TODO Auto-generated constructor stub
-	}() {
-		
-	}
+	} 
 
 }
--- Progra I/src/progra/algoritmos/tec/JsonStore.java
+++ Progra I/src/progra/algoritmos/tec/JsonStore.java
@@ -39,5 +39,3 @@ public class JsonStore implements Json{
 		
 	}
 	
-
-}
